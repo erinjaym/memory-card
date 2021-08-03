a68aa1e792b302c694764cdf4e8709e62d1ca80b
erinjaym <erin.jay.moyer@gmail.com>
2021-08-02 21:16:34 -0400

    Reworked code in Gameboard and App.js GameBoard now only displays content as a component should. App handles all of the game logic. Altered useEffect to display high score throughout the game and update it in real time. Added functions ShiftCards, SelectCard, and Selected card to make changes to state and score as game progresses. Game is now fully functional. Next Step is to remove redundancy in the code and deconstruct or rename a few things to make code easier to follow.

--- src/App.js
+++ src/App.js
@@ -11,48 +11,75 @@ const [flashCards, setFlashCards] = useState (FlashCardData);
 
 
 useEffect (() => {
-// maybe use .map to change all of the cards Location accordingly
-//setSlot1 ({source: Fire});
+  const setTheHighScore = () => {
+    if (highScore < score){
+      return setHighScore ( score );
+    }else {
+      return;
+    }
+  }
+  setTheHighScore();
 
 });
 
-const imageSelection = (img) => {
+const gameReset = () => {
+  setFlashCards (FlashCardData);
+  setScore (0);
 }
 
-// only called if image does not match prevously selected images 
-// each image object has a true false value assigned to track this ?
+
 const addPoints = () => {
   setScore (score + 1);
 }
 
-  // call after each click ?? after each onMount? 
-const setTheHighScore = (newScore) => {
-    if (highScore < newScore){
-      return setHighScore ( newScore );
-    }else {
-      return;
-    }
+function Randomizer () {
+  let min = Math.ceil(9);
+  let max = Math.floor(1);
+  return Math.floor(Math.random() * (max - min +1) + min);
 }
 
-const checkSelection = (imgObject) => {
-  if (imgObject.selected === false){
-    addPoints();
-    //setHighScore ? 
-    return; //need to mark imgObject as selected on return
-  }else{
-    // restart game 
+function shiftCards () {
+  let shiftBy = Randomizer();
+  let flashCardArray = flashCards;
+  for (let shiftTimes = 0; shiftTimes < shiftBy; shiftTimes ++){
+      flashCardArray.unshift(flashCardArray[8]);
+      flashCardArray.pop();
+  }
+  return setFlashCards(flashCardArray);
+}
+
+const selectCard = (cardId) => {
+    //may be issues due to how selected is set not in state? 
+  if (flashCards.[cardId].selected){
+    // do something to reset game?
+    alert ('you have failed your mission!');
+    return false;
+  }else {
+    console.log(flashCards[cardId].selected);
+    flashCards[cardId].selected = true; 
+    console.log(flashCards[cardId].selected);
+    setFlashCards (flashCards);
+    return true;
   }
 }
 
-const selectedImage = (imageObject) => {
-  // ...slot1 Source: Wind? 
+
+const selectedCard = (arrayId) => {
+  // change the specific card to selected
+  if (selectCard(arrayId)){
+    addPoints();
+    shiftCards();
+    return console.log('did eet');
+  }else {
+    alert('game over for you');
+    gameReset();
+  }
 }
 
   return (
     <div id="game-wrapper" className="game-wrapper">
-      
       <ScoreBoard score={score} highScore={highScore}/>
-      <GameBoard flashCardList={flashCards} selectedImage={selectedImage}//slot2={} slot3={} slot4={} slot5={} slot6={} slot7={} slot8={} slot9={}
+      <GameBoard flashCardList={flashCards} selectCard={selectedCard}
       /> 
     </div>
   );
